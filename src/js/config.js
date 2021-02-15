8035595672669b0166b9b750774f293c8209c993
VadymShlikhta <V_Shlikhta@ukr.net>
2021-02-15 20:41:19 +0200

    New commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const MODAL_CLOSE_SEC = 2.5;
+export const KEY = `717d8eae-f30d-4435-9a37-26af6900cdc5`;
